65637d455259aabe3ea830307a99258dede91458
Mikhail Tsarev <mvtsarev@yandex-team.ru>
2025-05-31 14:19:25 +0300

    lab9: ELK

--- src/main/java/com/example/todoservice/controller/TaskController.java
+++ src/main/java/com/example/todoservice/controller/TaskController.java
@@ -3,8 +3,11 @@ package com.example.todoservice.controller;
 import com.example.todoservice.db.ModelConverter;
 import com.example.todoservice.db.TaskDatabase;
 import com.example.todoservice.db.entity.TaskEntity;
+import com.example.todoservice.messaging.TaskConsumer;
 import com.example.todoservice.messaging.TaskProducer;
 import com.example.todoservice.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +20,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+    private static final Logger log = LoggerFactory.getLogger(TaskController.class);
 
     private final HashMap<Long, Task> tasks = new HashMap<>();
     private final TaskProducer taskProducer;
@@ -29,11 +33,13 @@ public class TaskController {
 
     @GetMapping
     public List<Task> getAllTasks() {
+        log.warn("get all tasks"); // WARN чисто ради разнообразия логов
         return taskDatabase.findAll().stream().map(ModelConverter::taskModel).toList();
     }
 
     @GetMapping("/{id}")
     public Task getTask(@PathVariable Long id) {
+        log.info("get task " + id.toString());
         Optional<TaskEntity> taskEntity = taskDatabase.findById(id);
         if (taskEntity.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found with id " + id.toString());
@@ -43,6 +49,7 @@ public class TaskController {
 
     @PostMapping
     public Task createTask(@RequestBody Task task) {
+        log.info("create task " + ModelConverter.taskEntity(task).toString());
         taskDatabase.save(ModelConverter.taskEntity(task));
         taskProducer.send(task);
         return task;
